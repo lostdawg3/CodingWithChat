f577f53bc0dd281157e93d7b8389fc7c6a1c1773
lostdawg3 <lostdawg3@yahoo.com>
2020-11-15 03:59:31 -0800

    update prod

--- CodingWithMitchChat/settings.py
+++ CodingWithMitchChat/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,16 +21,37 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k=yf)71t7*(v&cas*#ae=*md05f85_y9q8uyn1w)bb-tc^&=^t'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+###############################################
+# UNCCOMMENT THIS WHEN USING IN DEVELOPMENT
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'k=yf)71t7*(v&cas*#ae=*md05f85_y9q8uyn1w)bb-tc^&=^t'
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+###############################################
+
+###############################################
+# ADDED for PRODUCTION
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = ["206.189.223.242",]
+
+ROOT_URLCONF = f'{config("PROJECT_NAME")}.urls'
+
+WSGI_APPLICATION = f'{config("PROJECT_NAME")}.wsgi.application'
+
+ASGI_APPLICATION = f'{config("PROJECT_NAME")}.routing.application'
+
+###############################################
 
 if DEBUG: 
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # development ONLY
 
-ALLOWED_HOSTS = []
+###############################################
+# USED IN DEVELOPMENT
+# ALLOWED_HOSTS = []
+###############################################
 
 AUTH_USER_MODEL = "account.Account"
 AUTHENTICATION_BACKENDS = (
@@ -69,7 +92,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ChatServerPlayground.urls'
+###############################################
+# USED IN DEVELOPMENT
+#ROOT_URLCONF = 'ChatServerPlayground.urls'
+###############################################
 
 TEMPLATES = [
     {
@@ -87,9 +113,14 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ChatServerPlayground.wsgi.application'
+###############################################
+# USED IN DEVELOPMENT
+#WSGI_APPLICATION = 'ChatServerPlayground.wsgi.application'
 
-ASGI_APPLICATION = 'ChatServerPlayground.routing.application'
+# USED IN DEVELOPMENT
+#ASGI_APPLICATION = 'ChatServerPlayground.routing.application'
+###############################################
+
 
 CHANNEL_LAYERS = {
     'default': {
@@ -152,25 +183,75 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760      # 10 * 1024 * 1024 = 10MB
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+###############################################
+# USED IN DEVELOPEMENT
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#     os.path.join(BASE_DIR, 'media'),
+# ]
+
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+
+# TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
+
+# BASE_URL = "http://127.0.0.1:8000"
+
+###############################################
+
+
+###############################################
+# ADDED for PRODUCTION
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config("DB_NAME"),
+#         'USER': config("DB_USER"),
+#         'PASSWORD': config("DB_PASSWORD"),
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = config('AWS_LOCATION')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
 ]
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+TEMP = os.path.join(BASE_DIR, 'temp')
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
 
-BASE_URL = "http://127.0.0.1:8000"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'CodingWithMitch Team <noreply@codingwithmitch.com>'
 
-DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760      # 10 * 1024 * 1024 = 10MB
 
+BASE_URL = "http://206.189.223.242"
+
+###############################################
 
 
